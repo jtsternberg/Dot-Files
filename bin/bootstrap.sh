7b78f41239100c695883fe869ecc51fa3b8091c9
Justin Sternberg <justin@dsgnwrks.pro>
2014-12-10 15:34:21 -0500

    add bootstrap to bin

---
+++ bin/bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+git clone --recursive https://github.com/jtsternberg/Dot-Files.git ~/.dotfiles
+
+if [[ "$OSTYPE" =~ ^linux ]]; then
+	sudo apt-get -y install zsh tree tig wget
+
+	curl -L http://install.ohmyz.sh | sh
+	chsh -s $(which zsh)
+
+	cd ~/.dotfiles && chmod +x symdotfiles && symdotfiles
+fi
