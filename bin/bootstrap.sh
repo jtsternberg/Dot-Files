a2d325f85d4b5858cd368cc260530628fc3ba097
Justin Sternberg <justin@dsgnwrks.pro>
2014-12-10 15:47:19 -0500

    change order of scripts

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -7,8 +7,8 @@ git clone --recursive https://github.com/jtsternberg/Dot-Files.git ~/.dotfiles
 if [[ "$OSTYPE" =~ ^linux ]]; then
 	sudo apt-get -y install zsh tree tig wget
 
-	curl -L http://install.ohmyz.sh | sh
-	chsh -s $(which zsh)
-
 	cd ~/.dotfiles && chmod +x symdotfiles && sh symdotfiles
+	curl -L http://install.ohmyz.sh | sh
+
+	chsh -s $(which zsh)
 fi
