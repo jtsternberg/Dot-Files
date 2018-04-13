a85e5dcabbcb3d56b978499ae9435b1be9b7f2cd
Justin Sternberg <justin@dsgnwrks.pro>
2018-04-13 14:08:31 -0400

    Update my sites shortcut plugin

--- zsh-custom/plugins/jt-sites/jt-sites.plugin.zsh
+++ zsh-custom/plugins/jt-sites/jt-sites.plugin.zsh
@@ -1,7 +1,24 @@
 site() {
-	cd ~/vagrant/www/$1/htdocs/wp-content;
+	if [ -d ~/Sites/$1/wp-content ]; then
+		cd ~/Sites/$1/wp-content;
+	else
+		cd ~/Sites/$1/app/public/wp-content;
+	fi
 }
 _site() {
-	_files -W ~/vagrant/www/ -/;
+	_files -W ~/Sites/ -/;
 }
 compdef _site site
+
+# Autocompletes sites in my vagrant vvv directory.
+vvvsite() {
+	if [ -d ~/vagrant/www/$1/wp-content ]; then
+		cd ~/vagrant/www/$1/wp-content;
+	else
+		cd ~/vagrant/www/$1/htdocs/wp-content;
+	fi
+}
+_vvvsite() {
+	_files -W ~/vagrant/www/ -/;
+}
+compdef _vvvsite vvvsite
